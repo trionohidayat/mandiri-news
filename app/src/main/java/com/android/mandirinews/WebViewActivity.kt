9e3e7d61668b13c7ae609dd5114277c3756d37f5
trionohidayat3 <trionohidayat3@gmail.com>
2023-06-15 17:02:28 +0700

    View CHANGELOG Version 1.0.2

--- app/src/main/java/com/android/mandirinews/WebViewActivity.kt
+++ app/src/main/java/com/android/mandirinews/WebViewActivity.kt
@@ -1,7 +1,9 @@
 package com.android.mandirinews
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.android.mandirinews.databinding.ActivityWebViewBinding
 
@@ -9,6 +11,7 @@ class WebViewActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWebViewBinding
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWebViewBinding.inflate(layoutInflater)
@@ -16,10 +19,30 @@ class WebViewActivity : AppCompatActivity() {
 
         val url = intent.getStringExtra("url")
 
-        binding.webView.webViewClient = WebViewClient()
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.webViewClient = MyWebViewClient()
 
         if (url != null) {
             binding.webView.loadUrl(url)
+        }
+    }
+
+    inner class MyWebViewClient : WebViewClient() {
+        @Deprecated("Deprecated in Java")
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            // Setelah URL dimuat di WebView, tautan lain akan dibuka di WebView yang sama
+            if (url != null) {
+                view?.loadUrl(url)
+            }
+            return true
+        }
+    }
+
+    override fun onBackPressed() {
+        if (binding.webView.canGoBack()) {
+            binding.webView.goBack()
+        } else {
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
