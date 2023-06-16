372c9f08355163083f96f4f0fb0450939b30cc24
trionohidayat3 <trionohidayat3@gmail.com>
2023-06-16 13:48:08 +0700

    View CHANGELOG Version 1.2.1

--- app/src/main/java/com/android/mandirinews/WebViewActivity.kt
+++ app/src/main/java/com/android/mandirinews/WebViewActivity.kt
@@ -1,15 +1,19 @@
 package com.android.mandirinews
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import com.android.mandirinews.databinding.ActivityWebViewBinding
 
 class WebViewActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWebViewBinding
+    private lateinit var progressWeb: ProgressBar
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,10 @@ class WebViewActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val url = intent.getStringExtra("url")
+
+        progressWeb = binding.progressWeb
+
+        progressWeb.visibility = View.VISIBLE
 
         binding.webView.settings.javaScriptEnabled = true
         binding.webView.webViewClient = MyWebViewClient()
@@ -28,16 +36,39 @@ class WebViewActivity : AppCompatActivity() {
     }
 
     inner class MyWebViewClient : WebViewClient() {
+        private var loadingFinished = true
+        private var redirect = false
+
         @Deprecated("Deprecated in Java")
         override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-            // Setelah URL dimuat di WebView, tautan lain akan dibuka di WebView yang sama
+            if (!loadingFinished) {
+                redirect = true
+            }
+
+            loadingFinished = false
             if (url != null) {
                 view?.loadUrl(url)
             }
             return true
         }
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            loadingFinished = false
+            progressWeb.visibility = View.VISIBLE
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            if (!redirect) {
+                loadingFinished = true
+                progressWeb.visibility = View.GONE // Sembunyikan ProgressBar saat selesai
+            } else {
+                redirect = false
+            }
+        }
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         if (binding.webView.canGoBack()) {
             binding.webView.goBack()
